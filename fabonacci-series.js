5e1ce16206566b6c9223a1626a7f738bfbed5117
Tarun Nagpal <tarun.nagpal78@gmail.com>
2020-08-06 11:30:11 +0530

    fabonacci series updated

--- fabonacci-series.js
+++ fabonacci-series.js
@@ -1,26 +1,9 @@
-// 1, 1, 2, 3, 5, 8, 13
-
-
-var series = [1];
-
-for (let i = 1; i < 10; i++){
-    if(i == 1){
-        series.push(i);
-    }
-    var value = series[series.length - 1];
-    // console.log('value');
-    console.log(value);
-    // console.log('value');
-    // series.push(value + i);
-
-    // if(value > i){
-    //     break;
-    // }
-    // Get the Last element 
-
-    // Add the current element and push the record to series 
+var a = 1;
+var b = 2;
+var c;
+for(var i = 0; i<10; i++){
+    c = a + b;
+    a = b;
+    b = c;
+    console.log(c);
 }
-
-console.log('Final Series');
-console.log(series);
-console.log('Final Series');
